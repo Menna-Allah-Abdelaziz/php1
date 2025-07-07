b0ef16589f1475d14c2d9cfce2e2e325883a448f
Menna-Allah-Abdelaziz <abdelazizmena00@gmail.com>
2025-07-07 04:26:33 +0300

    Delete crud_bootstrap directory

--- crud_bootstrap/connect.php
+++
@@ -1,9 +0,0 @@
- <?php
-$host ="localhost";
-$user_name ="root";
-$password ="";
-$db ="crud";
-$connect = new mysqli($host ,$user_name ,$password ,$db);
-if($connect->connect_error)
-echo"connection failed" .$connect->connect_error;
-?>
